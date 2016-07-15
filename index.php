47dddb85e7834aee8f82b31c1fe843b05b929d68
kmkuzmich <1akuzichkin1@gmail.com>
2016-07-15 12:07:10 +0300

    stable 2016-07-15

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-error_reporting(E_ALL & ~E_WARNING & ~E_NOTICE & ~E_DEPRECATED);
+error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
 @ini_set('display_errors', false);
 if ($_SERVER['REMOTE_ADDR']=="78.152.169.139"){@ini_set('display_errors', true);}
 define('RD', dirname (__FILE__));
