ab278215ca20167b213ed0015113692dc419785d
kmKuzmich <1akuzichkin1@gmail.com>
2016-07-20 17:09:05 +0300

    added into ODBC_Class PostGre SQL methods

--- lib/odbc_class.php
+++ lib/odbc_class.php
@@ -1,68 +1,113 @@
 <?php
-class odb{
-	var $query='';
 
-	function query_lider($query)
-	{
-		if (!$this->db_lider) {
-			$this->connect_lider();
-		}
-		$this->r_lider = odbc_exec($this->db_lider, $query);
-		if (odbc_error()) {
-			$fp = fopen(RD . '/lib/odbc_errors/lider_error.txt', 'a+');
-			fwrite($fp, "Lider-> data=" . date("Y-m-d H:i:s") . ":" . odbc_errormsg($this->db_lider) . "\r\n" . $query . "\r\n");
-			fclose($fp);
-		}
-		return $this->r_lider;
-	}
+class odb
+{
+    var $query = '';
 
-	function connect_lider($try=0){
-		$this->auth_lider_param();
-		$this->db_lider=odbc_pconnect($this->source_lider,$this->username_lider,$this->password_lider);
-		if (!$this->db_lider){$try+=1;
-			if ($try<=10){
-				$this->connect_lider($try);
-			}
-		}
-	}
+    function query_lider($query)
+    {
+        if (!$this->db_lider) {
+            $this->connect_lider();
+        }
+        $this->r_lider = odbc_exec($this->db_lider, $query);
+        if (odbc_error()) {
+            $fp = fopen(RD . '/lib/odbc_errors/lider_error.txt', 'a+');
+            fwrite($fp, "Lider-> data=" . date("Y-m-d H:i:s") . ":" . odbc_errormsg($this->db_lider) . "\r\n" . $query . "\r\n");
+            fclose($fp);
+        }
+        return $this->r_lider;
+    }
 
-	function auth_lider_param()
-	{
-		$this->source_lider = 'Lider';
-		$this->username_lider = 'dba';
-		$this->password_lider = 'sql';
-	}
+    function connect_lider($try = 0)
+    {
+        $this->auth_lider_param();
+        $this->db_lider = odbc_pconnect($this->source_lider, $this->username_lider, $this->password_lider);
+        if (!$this->db_lider) {
+            $try += 1;
+            if ($try <= 10) {
+                $this->connect_lider($try);
+            }
+        }
+    }
 
-	function query_td($query){
-		$this->connect_td();
-		if(!$this->db_td){ $this->connect_td();}
-		$this->r_td =odbc_exec($this->db_td,$query);
-		if (odbc_error()){ 
-			if (substr_count(odbc_errormsg($this->db_td),"SQL0973N")==0){
-				$fp = fopen(RD.'/lib/odbc_errors/db2_error.txt', 'a+');
-				fwrite($fp, "DB2-> data=".date("Y-m-d H:i:s").":".odbc_errormsg($this->db_td)."\r\n".$query."\r\n");
-				fclose($fp);
-			}
-		}
-		return $this->r_td;
-	}
+    function auth_lider_param()
+    {
+        $this->source_lider = 'Lider';
+        $this->username_lider = 'dba';
+        $this->password_lider = 'sql';
+    }
 
-	function connect_td()
-	{
-		$this->auth_td_param();
-		$this->db_td = odbc_pconnect($this->source_td, $this->username_td, $this->passwordl_td);
-	}
+    function query_td($query)
+    {
+        $this->connect_td();
+        if (!$this->db_td) {
+            $this->connect_td();
+        }
+        $this->r_td = odbc_exec($this->db_td, $query);
+        if (odbc_error()) {
+            if (substr_count(odbc_errormsg($this->db_td), "SQL0973N") == 0) {
+                $fp = fopen(RD . '/lib/odbc_errors/db2_error.txt', 'a+');
+                fwrite($fp, "DB2-> data=" . date("Y-m-d H:i:s") . ":" . odbc_errormsg($this->db_td) . "\r\n" . $query . "\r\n");
+                fclose($fp);
+            }
+        }
+        return $this->r_td;
+    }
 
-	function auth_td_param()
-	{
-		$this->source_td = 'TD';
-		$this->username_td = 'dba';
-		$this->passwordl_td = 'sql';
-	}
+    function connect_td()
+    {
+        $this->auth_td_param();
+        $this->db_td = odbc_pconnect($this->source_td, $this->username_td, $this->passwordl_td);
+    }
 
-	function num_rows($res){$count=0;
-	    while($temp = odbc_fetch_into($res, $counter)){ $count++; }
-    	return $count;
-	}
+    function auth_td_param()
+    {
+        $this->source_td = 'TD';
+        $this->username_td = 'dba';
+        $this->passwordl_td = 'sql';
+    }
+
+// Returns Number of Rows in Query by passing through All rows
+// Realy it's not effective but is easy to use
+    function num_rows($res)
+    {
+        $count = 0;
+        while ($temp = odbc_fetch_into($res, $counter)) {
+            $count++;
+        }
+        return $count;
+    }
+
+// Connection to PostGreSQL ODBC Driver To DB PgLider
+    function auth_pg_param()
+    {
+        $this->source_pg = 'PgLider';
+        $this->username_pg = 'dba';
+        $this->passwordl_pg = 'sql';
+    }
+
+    function connect_pg()
+    {
+        $this->auth_pg_param();
+        $this->db_pg = odbc_pconnect($this->source_pg, $this->username_pg, $this->passwordl_pg);
+    }
+
+    function query_pg($query)
+    {
+        $this->connect_pg();
+        if (!$this->db_pg) {
+            $this->connect_pg();
+        }
+        $this->r_pg = odbc_exec($this->db_pg, $query);
+        if (odbc_error()) {
+            if (substr_count(odbc_errormsg($this->db_pg), "SQL0973N") == 0) {
+                $fp = fopen(RD . '/lib/odbc_errors/pg_error.txt', 'a+');
+                fwrite($fp, "DB2-> data=" . date("Y-m-d H:i:s") . ":" . odbc_errormsg($this->db_pg) . "\r\n" . $query . "\r\n");
+                fclose($fp);
+            }
+        }
+        return $this->r_pg;
+    }
 }
+
 ?>
